0780f0bcd4d0bd697ed40cde36af8f223edb1386
cristian <cplop95@gmail.com>
2025-11-27 02:42:36 +0100

    use PHP

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 PORT=${PORT:-8080}
-sed "s/\${PORT}/$PORT/g" /app/nginx.conf > /tmp/nginx.conf
-mv /tmp/nginx.conf /app/nginx.conf
-php-fpm -y /app/php-fpm.conf &
-sleep 2
-nginx -c /app/nginx.conf -g 'daemon off;'
+
+echo "Starting PHP server on port $PORT..."
+
+php -S 0.0.0.0:$PORT -t . index.php
 
