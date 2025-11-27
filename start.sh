922ad59b2219ab6769b3e420311ce1f04db865ad
cristian <cplop95@gmail.com>
2025-11-27 03:11:13 +0100

    Use exec

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ echo "Starting PHP server on port $PORT..."
 echo "PORT environment variable: $PORT"
 echo "Listening on 0.0.0.0:$PORT"
 
-php -S 0.0.0.0:$PORT -t . server.php
+exec php -S 0.0.0.0:$PORT -t . server.php
 
