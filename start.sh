f8502ca18613eaca8243e70e917034f72409f839
cristian <cplop95@gmail.com>
2025-11-27 02:24:15 +0100

    Fix nginx HTTPS

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-export PORT=${PORT:-8080}
-sed -i "s/listen 8080;/listen $PORT;/" /app/nginx.conf
+PORT=${PORT:-8080}
+sed "s/\${PORT}/$PORT/g" /app/nginx.conf > /tmp/nginx.conf
+mv /tmp/nginx.conf /app/nginx.conf
 php-fpm -y /app/php-fpm.conf &
+sleep 2
 nginx -c /app/nginx.conf -g 'daemon off;'
 
